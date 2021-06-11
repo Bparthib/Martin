c6b8bfbe61e093a4b615dcd4ada2fa8278b6ae64
Bparthib <bparthib77@gmail.com>
2021-06-11 18:46:10 +0600

    new add

--- js/app.js
+++ js/app.js
@@ -1,9 +1,18 @@
 $(function () {
+  //Navbar animation
   $(window).on("scroll", function () {
     if ($(window).scrollTop() > 0) {
       $("#navbar").addClass("stickyNavbar");
     } else {
       $("#navbar").removeClass("stickyNavbar");
     }
+  });
+
+  //Co-founder section animation
+  $("#co-founder").slick({
+    arrows: false,
+    dots: true,
+    centerMode: true,
+    slideToShow: 1
   });
 });
