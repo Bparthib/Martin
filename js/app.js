f8159eea4fefbda5b909cfd7d92727deb2a874c3
Bparthib <bparthib77@gmail.com>
2021-06-16 11:54:53 +0600

    new add

--- js/app.js
+++ js/app.js
@@ -13,6 +13,45 @@ $(function () {
     autoplay: 2000,
     arrows: false,
     dots: true,
-    dotsClass: "slider_dots"
+    dotsClass: "slider_dots",
+  });
+
+  //BackToTop Button
+  $(window).on("scroll", function () {
+    if ($(window).scrollTop() > 800) {
+      $("#backToTop").fadeIn("slow");
+    } else {
+      $("#backToTop").fadeOut("slow");
+    }
+  });
+
+  $("#backToTop").on("click", function () {
+    $("html,body").animate(
+      {
+        scrollTop: 0,
+      },
+      2000
+    );
+  });
+
+  //Scroll Smooth
+  $("a").on("click", function () {
+    e.preventDefault();
+
+    if (this.hash !== "") {
+      var hash = this.hash;
+
+      $("html,body").animate(
+        {
+          scrollTop: $(hash).offset().top - 60,
+        },
+        2000
+      );
+    }
+  });
+
+  //preloader
+  $(window).on("load", function () {
+    $("#preloader").fadeOut(1000);
   });
 });
